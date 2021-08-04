4f82a0f183c5d6c8d65e7b7cc99279c1c2d593f2
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-08-04 14:58:42 +0100

    Extracting Route Params

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,8 +1,15 @@
+import { useParams } from 'react-router-dom'
+
 const ProductDetail = () => {
+  const params = useParams()
+  console.log(params.productId)
+
   return (
-    <section>
-      <h3>Product Detail</h3>
-    </section>
+    <sectionY>
+      <h1>Product Detail</h1>
+      <h2>{params.productId}</h2>
+    </sectionY>
   )
 }
+
 export default ProductDetail
